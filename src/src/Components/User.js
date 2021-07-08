135af2060c8812ab7498eae51f461a192717261b
effzedwhy <fatemazakir@gmail.com>
2021-07-08 13:42:40 +0100

    added gender feature

---
+++ src/src/Components/User.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./User.css";
+
+const User = ({ title, first, last, country, email, age, phone, photo }) => {
+  return (
+    <li>
+      <h1>
+        {title}&nbsp;{first}&nbsp;{last}
+      </h1>
+      <div className="all-info">
+        <div className="photo">
+          <img src={photo} alt="user"></img>
+        </div>
+        <div className="info">
+          <p>
+            <span>Age:</span> {age}
+          </p>
+          <p>
+            <span>Country:</span> {country}
+          </p>
+          <p>
+            <span>Email:</span> {email}
+          </p>
+          <p>
+            <span>Phone Number:</span> {phone}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default User;
